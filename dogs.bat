ebf1d856f4541514b62d6afa86b845ff8593300d
Patrick Parker <patrickbardinparker@gmail.com>
2021-04-18 16:16:51 -0700

    Add files via upload
    initial

---
+++ dogs.bat
@@ -0,0 +1,12 @@
+
+set root=C:\Users\user\anaconda3
+
+call %root%\Scripts\activate.bat %root%
+
+call cd C:\users\user\desktop\dog_classifier
+
+call conda activate tfdogs
+
+call voila dog_breed_classifier_r1.ipynb
+
+pause
